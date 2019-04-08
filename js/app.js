31163458e5e3fb35a88658e50a85a691a3d515cd
Scott Reyell <smreyell@gmail.com>
2019-04-07 22:55:51 -0700

    hooked up omdb api and displaying movie info to the client

---
+++ js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(() => {
+  $('#searchForm').on('submit', (e) => {
+    let searchText = $('#searchText').val();
+
+    getMovie(searchText);
+
+    e.preventDefault();
+  });
+});
+
+function getMovie(searchText){
+  axios.get('http://www.omdbapi.com/?s='+searchText+'&apikey=cfbc6a0c')
+    .then((response) => {
+      console.log(response);
+      let movies = response.data.Search;
+      let output = '';
+      $.each(movies, (index, movie) => {
+        output += `
+          <div class="col-md-3">
+            <div class="well text-center">
+              <img src="${movie.Poster}">
+              <h5>${movie.Title}</h5>
+              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+            </div>
+          </div>
+        `
+      });
+
+      $('#movies').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
